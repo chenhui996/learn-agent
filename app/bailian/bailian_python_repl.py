b94df9a5297b6ad4583f92d5d5b25189f748489a
chenhui996 <chen562450669@qq.com>
2025-11-04 16:21:53 +0800

    feat: 使用 python repl 创建官网（5-4）

--- app/bailian/bailian_python_repl.py
+++ app/bailian/bailian_python_repl.py
@@ -1,11 +1,60 @@
-from langchain_experimental.utilities import PythonREPL
+from langchain_experimental.tools.python.tool import PythonREPLTool
+from langchain.agents import initialize_agent, AgentType
 
-# 01. 生成 python_repl 实例
-python_repl = PythonREPL()
+from app.bailian.common import llm
+from langchain.prompts import PromptTemplate
 
-# 02. 尝试执行 python 代码
-ret =python_repl.run("print(1+1)")
+# 01. 既然会用 repl 了，那，用 repl 写一个 PythonREPL 的 tools 试试
 
-print(ret)
+# 02. 定义工具
+tools = [PythonREPLTool()]
 
-# 03. 既然会用 repl 了，那，用 repl 写一个 PythonREPL 的 tools 试试
+# 03. 定义工具名称
+tool_names = ["PythonREPLTool"]
+
+# 04. 创建智能体
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+)
+
+# 05. 创建提示词的模版
+prompt_template = PromptTemplate.from_template(template="""
+尽你所能回答用户的问题或执行用户的命令，你可以使用以下工具：[{tool_names}]
+--
+请按照以下格式进行思考：
+```
+# 思考的过程
+- 问题：你必须回答的问题
+- 思考：你考虑应该怎么做
+- 行动：要采取的行动，应该是[{tool_names}]中的一个
+- 行动输入：行动的输入
+- 观察：行动的结果
+...（这个思考/行动/行动输入/观察可以重复N次）
+# 最终答案
+对原始输入问题的最终答案
+```
+--
+注意：
+- PythonREPLTool工具的入参是python代码，不允许添加 ```python 或 ```py 等标记
+--
+要求：{input}
+""")
+
+# 06. 生成提示词
+prompt = prompt_template.format(
+    tool_names=", ".join(tool_names),
+    input="""
+要求：
+1. 向 /Users/chenhui/Downloads/aiAgent/ai-agent-test/.temp 目录下下入一个新文件，名称为 `index.html` 的文件。
+2. 写一个在线教育产品的官网，包含3个tab，分别是：首页、实战课、体系课和关于我们
+3. 首页展示3个模块，分别是：热门课程、上新课程、爆款课程
+4. 关于我们展示平台的联系方式等基本信息
+"""
+)
+
+# print(prompt)
+
+resp = agent.invoke(prompt)
