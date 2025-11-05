91fcf3d5bb76fcd35fc1dfb60e174fd13034458f
chenhui996 <chen562450669@qq.com>
2025-11-05 19:16:51 +0800

    feat: 接入高德地图 mcp -> 实现各种类型的地图问答（6-5）

--- app/bailian/common.py
+++ app/bailian/common.py
@@ -11,7 +11,8 @@ from pydantic import BaseModel, Field
 ############################################################################################################
 
 llm = ChatOpenAI(
-    model="qwen3-max",
+    # model="qwen3-max",
+    model="qwen3-235b-a22b",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
     api_key=StrictStr("sk-cfb32d3791ba47cb915dfed7fe758cd9"),
     streaming=True,
--- app/mcp/amap/amap_mcp_client.py
+++ app/mcp/amap/amap_mcp_client.py
@@ -1,11 +1,15 @@
 import os
 import asyncio
 
+from langchain.agents import initialize_agent, AgentType
+from langchain_core.prompts import PromptTemplate
 from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from app.bailian.common import llm
 
 
 async def create_amap_mcp_client():
-    amap_key = os.environ.get("AMAP_KEY")
+    amap_key = os.getenv("AMAP_KEY")
     mcp_config = {
         "amap": {
             "url": f"https://mcp.amap.com/sse?key={amap_key}",
@@ -14,10 +18,32 @@ async def create_amap_mcp_client():
     }
 
     client = MultiServerMCPClient(mcp_config)
-    print(client)
+    # print(client)
 
     tools = await client.get_tools()
-    print(tools)
+    # print(tools)
+
+    return client, tools
 
 
-asyncio.run(create_amap_mcp_client())
+async def create_and_run_agent():
+    client, tools = await create_amap_mcp_client()
+
+    agent = initialize_agent(
+        llm=llm,
+        tools=tools,
+        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True
+    )
+
+    prompt_template = PromptTemplate.from_template("你是一个智能助手，可以调用高德 MCP 工具。\n\n问题: {input}")
+    prompt = prompt_template.format(input="规划北京南站到北京望京SOHO的公交或地铁路线")
+    print(prompt)
+
+    resp = await agent.ainvoke(prompt)
+    print(resp)
+
+    return resp
+
+
+asyncio.run(create_and_run_agent())
