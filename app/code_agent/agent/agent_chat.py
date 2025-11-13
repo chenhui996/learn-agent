cd48a9c533e556f6e43bb175d52dc80377b177db
chenhui996 <chen562450669@qq.com>
2025-11-13 18:02:04 +0800

    feat: 使用 redis 实现记忆存储（9-3）

--- app/code_agent/agent/agent_chat.py
+++ app/code_agent/agent/agent_chat.py
@@ -1,6 +1,6 @@
 # 目标：让 agent 具有记忆能力（memory）
 from langchain_core.runnables import RunnableConfig
-from langgraph.checkpoint.memory import MemorySaver # 提供 内存存储 能力
+from langgraph.checkpoint.redis import RedisSaver # 提供 redis 存储能力
 from langgraph.prebuilt import create_react_agent # 创建智能体
 
 from app.code_agent.model.qwen import llm_qwen
@@ -10,17 +10,19 @@ from app.code_agent.tools.file_tools import file_tools
 # 创建智能体：
 def create_agent():
     # 实例化一个 memory 对象
-    memory = MemorySaver()
 
-    # 实例化一个智能体
-    agent = create_react_agent(
-        model=llm_qwen,
-        tools=file_tools,
-        checkpointer=memory,
-        debug=True
-    )
+    with RedisSaver.from_conn_string("redis://localhost:6379") as memory:
+        memory.setup()
 
-    return agent
+        # 实例化一个智能体
+        agent = create_react_agent(
+            model=llm_qwen,
+            tools=file_tools,
+            checkpointer=memory,
+            debug=True
+        )
+
+        return agent
 
 
 
@@ -29,17 +31,17 @@ def run_agent():
 
     agent = create_agent()
 
-    res = agent.invoke(input={"messages": [("user", "我是 cain")]}, config=config)
+    res = agent.invoke(input={"messages": [("user", "我们刚才聊了什么？")]}, config=config)
 
     print("=" * 60)
     print(res)
     print("=" * 60)
 
-    res = agent.invoke(input={"messages": [("user", "我是谁？")]}, config=config)
-
-    print("=" * 60)
-    print(res)
-    print("=" * 60)
+    # res = agent.invoke(input={"messages": [("user", "我是谁？")]}, config=config)
+    #
+    # print("=" * 60)
+    # print(res)
+    # print("=" * 60)
 
 if __name__ == "__main__":
     run_agent()
