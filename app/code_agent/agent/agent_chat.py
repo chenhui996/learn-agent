468c6529e728b6486a96577ca4e07de4bb7b63a1
chenhui996 <chen562450669@qq.com>
2025-11-14 11:12:16 +0800

    feat: 使用 mongodb 实现记忆存储（9-4）

--- app/code_agent/agent/agent_chat.py
+++ app/code_agent/agent/agent_chat.py
@@ -1,19 +1,22 @@
 # 目标：让 agent 具有记忆能力（memory）
+import os
+
 from langchain_core.runnables import RunnableConfig
-from langgraph.checkpoint.redis import RedisSaver # 提供 redis 存储能力
-from langgraph.prebuilt import create_react_agent # 创建智能体
+# from langgraph.checkpoint.memory import MemorySaver # 提供 内存 存储能力
+from langgraph.checkpoint.redis import RedisSaver  # 提供 redis 存储能力
+from langgraph.checkpoint.mongodb import MongoDBSaver  # 提供 mongodb 存储能力
+from langgraph.prebuilt import create_react_agent  # 创建智能体
 
 from app.code_agent.model.qwen import llm_qwen
 from app.code_agent.tools.file_tools import file_tools
 
 
 # 创建智能体：
-def create_agent():
-    # 实例化一个 memory 对象
+def run_agent():
+    MONGODB_URI = os.environ.get("MONGODB_URI")
+    MONGODB_DB = os.environ.get("MONGODB_DB")
 
-    with RedisSaver.from_conn_string("redis://localhost:6379") as memory:
-        memory.setup()
-
+    with MongoDBSaver.from_conn_string(MONGODB_URI, MONGODB_DB) as memory:
         # 实例化一个智能体
         agent = create_react_agent(
             model=llm_qwen,
@@ -22,27 +25,22 @@ def create_agent():
             debug=True
         )
 
-        return agent
+        config = RunnableConfig(configurable={"thread_id": "1"})
 
+        # res = agent.invoke(input={"messages": [("user", "我是 cain，你好")]}, config=config)
+        #
+        # print("=" * 60)
+        # print(res)
+        # print("=" * 60)
 
+        res = agent.invoke(input={"messages": [("user", "ok，我来考考你，我是谁？")]}, config=config)
 
-def run_agent():
-    config = RunnableConfig(configurable={"thread_id": "1"})
+        print("=" * 60)
+        print(res)
+        print("=" * 60)
 
-    agent = create_agent()
+        memory.close()
 
-    res = agent.invoke(input={"messages": [("user", "我们刚才聊了什么？")]}, config=config)
-
-    print("=" * 60)
-    print(res)
-    print("=" * 60)
-
-    # res = agent.invoke(input={"messages": [("user", "我是谁？")]}, config=config)
-    #
-    # print("=" * 60)
-    # print(res)
-    # print("=" * 60)
 
 if __name__ == "__main__":
     run_agent()
-
